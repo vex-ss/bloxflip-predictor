a8af0de588a1070023d51f6c1f5eafe2e9d031bb
vex-ss <104242556+vex-ss@users.noreply.github.com>
2022-11-13 15:51:10 -0800

    Add files via upload

---
+++ setup.bat
@@ -0,0 +1,15 @@
+@echo off
+color 0A
+cls
+echo Downloading Modules...
+pip install aiohttp==3.8.1
+pip install bloxflippredictor==3.1.4
+pip install colorama==0.4.5
+pip install discord==2.0.0
+pip install discord.py==2.0.1
+pip install selenium==4.4.3
+pip install time
+pip install os
+pip install math
+cls
+python main.py
